40e7bc57244d866ee49c5bd01bd1659619f6880f
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-26 15:39:38 +0100

    feat: implement task management functionality with add, delete, and edit options

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,66 @@
+import { useState } from "react"
 export default function Main() {
+
+    const [tasklist, setTasklist] = useState([])
+    const [task, setTask] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (task === "") {
+            alert("Task cannot be empty")
+        } else {
+            setTasklist([...tasklist, task])
+        }
+    }
     return (
         <>
             <main>
                 <div className="container ">
                     <div className="row ">
-                        <div className="col-6">
-                            <form typeof="submit">
+                        <div className="col-6 mt-5">
+                            <form onSubmit={handleSubmit}>
+                                <div className="container d-flex gap-1">
+
+                                    <input type="text"
+                                        name="newTask"
+                                        id="newTask"
+                                        className=" input-group-text"
+                                        placeholder="Inserisci la nuova task"
+                                        value={task}
+                                        onChange={(e) => setTask(e.target.value)} />
+                                    <button
+                                        type="submit"
+                                        className="btn btn-primary "
+
+                                    >Add Task</button>
+                                </div>
 
                             </form>
                         </div>
-                        <div className="col-6"> a</div>
+                        <div className="col-6 mt-5">
+                            <div className="card p-4">
 
+                                {
+                                    tasklist.map((item, index) => (
+
+                                        <div key={index} className="item d-flex justify-content-between  ">
+                                            <div className="text_container">{item}</div>
+                                            <div className="btncontainer mb-3 ">
+                                                <div className="btn btn-primary me-2 ">Delete</div>
+                                                <div className="btn btn-primary ">Edit</div>
+                                            </div>
+                                        </div>
+
+                                    ))
+
+
+
+                                }
+                            </div>
+                        </div>
                     </div>
-
-
                 </div>
             </main>
-
-
         </>
 
 
